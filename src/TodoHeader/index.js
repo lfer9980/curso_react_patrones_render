05e4b3277f221371d2ae48ec4c82576ae1488845
lfer9980 <luis_fernandez99@hotmail.com>
2022-03-20 22:32:18 -0700

    :art: [CLASE 11] React.Children y React.cloneElement

--- src/TodoHeader/index.js
+++ src/TodoHeader/index.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-function TodoHeader({children}) {
-	return(
+function TodoHeader({ children, loading }) {
+
+	return (
 		<header>
-			{children}
+			{
+				React.Children
+					.toArray(children)
+					.map(child => (
+						React.cloneElement(child, { loading: loading })
+						)
+					)
+			}
 		</header>
 	);
 }
